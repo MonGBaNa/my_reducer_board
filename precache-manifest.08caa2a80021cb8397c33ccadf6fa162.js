0042717589c0eb5a89d9dea043063fb0c7e6c255
MonG_Bana <jjasu0826@gmail.com>
2020-04-24 19:24:40 +0900

    Updates

---
+++ precache-manifest.08caa2a80021cb8397c33ccadf6fa162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1daecc8744a3ec755961ae34c47aff68",
+    "url": "/my_reducer_board/index.html"
+  },
+  {
+    "revision": "ebdb5f20bacb79dac26a",
+    "url": "/my_reducer_board/static/css/main.2d684372.chunk.css"
+  },
+  {
+    "revision": "6341c670c07db75939c2",
+    "url": "/my_reducer_board/static/js/2.c69f4130.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_reducer_board/static/js/2.c69f4130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdb5f20bacb79dac26a",
+    "url": "/my_reducer_board/static/js/main.312e8b1d.chunk.js"
+  },
+  {
+    "revision": "33bdc54d0a02d4b580f4",
+    "url": "/my_reducer_board/static/js/runtime-main.7d186e1e.js"
+  }
+]);
