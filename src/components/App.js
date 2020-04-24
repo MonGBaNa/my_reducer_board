b9fb966505aeb481c3a14f8a8d8727b9a00af2e3
MonG_Bana <jjasu0826@gmail.com>
2020-04-24 19:53:40 +0900

    change to hash router

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ const App = (props) => {
   },[])
   return (
     <>
-      <Router basename='/my_reducer_board'>
+      <Router basename='/my_reducer_board/'>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/write" component={Write} />
