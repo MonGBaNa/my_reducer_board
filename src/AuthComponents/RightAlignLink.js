00e8f8213c27a19dee452c4948922b01e7571525
MonG_Bana <jjasu0826@gmail.com>
2020-05-04 18:11:23 +0900

    a to link

--- src/AuthComponents/RightAlignLink.js
+++ src/AuthComponents/RightAlignLink.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Aligner = styled.div``;
 
 const RightAlignLink = ({to, children}) => {
     return (
         <Aligner className="mt-2 text-right text-gray-700 underline">
-            <a href={to}>{children}</a>
+            <Link to={to}>
+                {children}
+            </Link>
         </Aligner>
     );
 }
